76ca3f6440b1a01135fa367b648db9ab2a5a3aaf
ygtxr1997 <gavingreenson@icloud.com>
2021-11-06 09:27:04 +0800

    update config

--- config.py
+++ config.py
@@ -7,12 +7,12 @@ cfg.sample_rate = 1
 cfg.fp16 = True
 cfg.momentum = 0.9
 cfg.weight_decay = 5e-4
-cfg.batch_size = 196  # 128
+cfg.batch_size = 128  # 128
 cfg.lr = 2e-4  # 0.1 for batch size is 512
 cfg.nw = 20
 
 """ Setting EXP ID """
-cfg.exp_id = 31124
+cfg.exp_id = 30122
 cfg.output = "tmp_" + str(cfg.exp_id)
 print('output path: ', cfg.output)
 
@@ -22,7 +22,7 @@ ft_set.dim = 384
 ft_set.depth = 4
 ft_set.heads = 8
 ft_set.dim_head = 64
-ft_set.mlp_dim = 192
+ft_set.mlp_dim = 256
 ft_set.emb_dropout = 0.1
 ft_set.dropout = 0.1
 
@@ -38,17 +38,17 @@ st_set.dropout = 0.2
 
 """ Setting for Model DualPathTransformer"""
 dp_set = edict()
-dp_set.dim = 256
+dp_set.dim = 128
 dp_set.depth = 1
-dp_set.heads_id = 6
-dp_set.heads_oc = 4
+dp_set.heads_id = 4
+dp_set.heads_oc = 2
 dp_set.dim_head_id = 64
 dp_set.dim_head_oc = 32
-dp_set.mlp_dim_id = 192
+dp_set.mlp_dim_id = 128
 dp_set.mlp_dim_oc = 128
-dp_set.emb_dropout = 0.1
-dp_set.dropout_id = 0.1
-dp_set.dropout_oc = 0.2
+dp_set.emb_dropout = 0.2
+dp_set.dropout_id = 0.2
+dp_set.dropout_oc = 0.3
 
 cfg.model_set = dp_set
 print(cfg.model_set)
